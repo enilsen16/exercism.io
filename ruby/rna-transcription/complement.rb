7771393b3d26ca6e2c3adfa4468e4750e342be3b
Erik Nilsen <enilsen16@live.com>
2014-08-13 18:15:41 -0700

    first working solution

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,14 +1,34 @@
 class Complement
 
   def self.of_dna(dna)
-    if dna == 'C'
-      dna = 'G'
-    elsif dna == 'G'
-      dna = 'C'
-    elsif dna == 'T'
-      dna = 'A'
-    elsif dna == 'A'
-      dna = 'U'
+    rna = ""
+    dna.each_char do |i|
+      if i == "C"
+        rna << "G"
+      elsif i == "G"
+        rna << "C"
+      elsif i == "T"
+        rna << "A"
+      elsif i == "A"
+        rna << "U"
+      end
     end
+    rna
+  end
+
+  def self.of_rna(rna)
+    dna = ""
+    rna.each_char do |i|
+      if i == "C"
+        dna << "G"
+      elsif i == "G"
+        dna << "C"
+      elsif i == "U"
+        dna << "A"
+      elsif i == "A"
+        dna << "T"
+      end
+    end
+    dna
   end
 end
